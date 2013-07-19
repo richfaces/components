1b72a62e9b23d65d223407ca1ab7bd1a9e918bbd
jigg4 <jwitowski@gmx.de>
2013-07-19 10:39:56 +0200

    RF-11088: Addded: arrow-key navigation in the popup of rich:select
    Pressing up/down arrow-key will now call a new method which selects the current marked item and sets the label into the input.

--- rich/src/main/resources/META-INF/resources/org.richfaces/select.js
+++ rich/src/main/resources/META-INF/resources/org.richfaces/select.js
@@ -167,6 +167,7 @@
                             this.__showPopup();
                         } else {
                             this.list.__selectNext();
+                            this.__setCurrentSelectToInput();
                         }
                         break;
 
@@ -174,6 +175,7 @@
                         e.preventDefault();
                         if (visible) {
                             this.list.__selectPrev();
+                            this.__setCurrentSelectToInput();
                         }
                         break;
 
@@ -268,6 +270,24 @@
                     $(document.getElementById(this.id + "Items")).empty().append(items);
                     this.__save();
                 }
+            },
+
+            __setCurrentSelectToInput : function() {
+                var item;
+                item = this.list.items.eq(this.list.index);
+
+                var key = $(item).attr("id");
+                var label;
+                $.each(this.clientSelectItems, function() {
+                    if (this.id == key) {
+                        label = this.label;
+                        return false;
+                    }
+                });
+                this.__setValue(label);
+                this.__save();
+
+                this.invokeEvent.call(this, "selectitem", document.getElementById(this.id));
             },
 
             __updateItemsFromCache: function(value) {
